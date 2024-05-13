741d398437c1f81c6e535e904d21a24cc6a8b8c5
Baptiste Dubillaud <baptiste.dubillaud@gmail.com>
2024-05-14 00:07:30 +0200

    cleaned body class

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={inter.className} style={{margin: 0, padding: 0, width: "100vw"}}>
+            <body className={inter.className}>
                 <NavigationBarComponent/>
                 {children}
                 <FooterComponent/>
