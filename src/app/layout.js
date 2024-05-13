f9c55d9bf200df55c8f70e932c128606b2027af7
Baptiste Dubillaud <baptiste.dubillaud@gmail.com>
2024-05-13 23:46:29 +0200

    nav bar first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,21 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavigationBarComponent from "@/components/core/navigationBar/NavigationBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "B. Dubillaud - Software engineer",
+    title: "Hi, I'm Baptiste!",
     description: "My personal website built to present my personal projetcs and contributions.",
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <NavigationBarComponent/>
+                {children}
+            </body>
         </html>
     );
 }
---
+++ src/components/core/navigationBar/NavigationBar.js
@@ -0,0 +1,19 @@
+
+
+export default function NavigationBarComponent({}) {
+
+    return (
+        <div>
+            <div>
+                Hi, I'm Baptiste!
+            </div>
+            <div>
+                <span>Home</span>
+                <span>Resume</span>
+                <span>Portfolio</span>
+                <span>Blog</span>
+                <span>Contact Me</span>
+            </div>
+        </div>
+    )
+}
