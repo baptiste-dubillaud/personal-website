fa0de463512281f45371339f097ab26936ea818f
Baptiste Dubillaud <baptiste.dubillaud@gmail.com>
2024-05-14 00:04:16 +0200

    Added base footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavigationBarComponent from "@/components/core/navigationBar/NavigationBar";
+import FooterComponent from "@/components/core/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,9 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <body className={inter.className} style={{margin: 0, padding: 0, width: "100vw"}}>
                 <NavigationBarComponent/>
                 {children}
+                <FooterComponent/>
             </body>
         </html>
     );
---
+++ src/components/core/footer/Footer.js
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css"
+
+export default function FooterComponent({}) {
+
+    return (
+        <div className={styles.footer_container}>
+            Developed in 2024 by Baptiste D.
+        </div>
+    )
+}
