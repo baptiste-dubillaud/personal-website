035f85399c7b1e94c1fe55f9e468c28240e1cf96
Baptiste Dubillaud-Etchegorry <baptiste.dubillaud@gmail.com>
2024-05-15 23:25:08 +0200

    nav bar design v1

--- src/components/core/navigationBar/NavigationBar.js
+++ src/components/core/navigationBar/NavigationBar.js
@@ -1,18 +1,33 @@
+'use client'
 
+import styles from "@/components/core/navigationBar/NavigationBar.module.css"
+
+import { useRouter } from "next/navigation"
+
+const NavLinkComponent = ({name, path}) => {
+
+    const router = useRouter()
+
+    return (
+        <div className={styles.nav_link} onClick>
+            {name}
+        </div>
+    )
+}
 
 export default function NavigationBarComponent({}) {
 
     return (
-        <div>
-            <div>
+        <div className={styles.nav_bar_container}>
+            <div className={styles.hello_message}>
                 Hi, I'm Baptiste!
             </div>
-            <div>
-                <span>Home</span>
-                <span>Resume</span>
-                <span>Portfolio</span>
-                <span>Blog</span>
-                <span>Contact Me</span>
+            <div className={styles.links_container}>
+                <NavLinkComponent name="Home" path="/"/>
+                <NavLinkComponent name="Resume" path="/resume"/>
+                <NavLinkComponent name="Portfolio" path="/portfolio"/>
+                <NavLinkComponent name="Blog" path="/blog"/>
+                <NavLinkComponent name="Contact me" path="/contact"/>
             </div>
         </div>
     )
