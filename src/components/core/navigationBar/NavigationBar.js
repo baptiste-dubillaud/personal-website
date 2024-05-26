acb542cb1800825a7cfa39edcaa3918c906d5bb0
Baptiste Dubillaud-Etchegorry <baptiste.dubillaud@gmail.com>
2024-05-26 12:16:03 +0200

    improve navigation bar

--- src/components/core/navigationBar/NavigationBar.js
+++ src/components/core/navigationBar/NavigationBar.js
@@ -2,32 +2,32 @@
 
 import styles from "@/components/core/navigationBar/NavigationBar.module.css"
 
-import { useRouter } from "next/navigation"
-
-const NavLinkComponent = ({name, path}) => {
-
-    const router = useRouter()
-
-    return (
-        <div className={styles.nav_link} onClick>
-            {name}
-        </div>
-    )
-}
+import { useRouter, usePathname } from "next/navigation"
 
 export default function NavigationBarComponent({}) {
+
+    const router = useRouter()
+    const pathname = usePathname()
+
+    const NavLinkComponent = ({name, path}) => {
+    
+        return (
+            <div className={pathname != path ? `${styles.nav_link_text} ${styles.nav_link}`: `${styles.nav_link_text} ${styles.nav_link_selected}`} onClick={() => router.push(path)}>
+                {name}
+            </div>
+        )
+    }
 
     return (
         <div className={styles.nav_bar_container}>
             <div className={styles.hello_message}>
-                Hi, I'm Baptiste!
+                Hi, I'm <span className={styles.hello_message_name}>Baptiste</span>!
             </div>
             <div className={styles.links_container}>
                 <NavLinkComponent name="Home" path="/"/>
                 <NavLinkComponent name="Resume" path="/resume"/>
                 <NavLinkComponent name="Portfolio" path="/portfolio"/>
                 <NavLinkComponent name="Blog" path="/blog"/>
-                <NavLinkComponent name="Contact me" path="/contact"/>
             </div>
         </div>
     )
