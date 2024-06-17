1c8364145626724a77b49e932801ea5cd0d821f4
Baptiste Dubillaud <baptiste.dubillaud@gmail.com>
2024-06-17 17:57:16 +0200

    Improve footer

--- src/components/core/footer/Footer.js
+++ src/components/core/footer/Footer.js
@@ -4,7 +4,9 @@ export default function FooterComponent({}) {
 
     return (
         <div className={styles.footer_container}>
-            Developed in 2024 by Baptiste D.
+
+            <span>Developed in <span className={styles.bold_text}>2024</span> by</span>
+            <span className={`${styles.orange_text} ${styles.bold_text}`}>Baptiste DUBILLAUD</span>
         </div>
     )
 }
