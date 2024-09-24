f13f2f3f5db172c742e083489fc73e8fa8f351e9
Baptiste Dubillaud <baptiste.dubillaud@gmail.com>
2024-09-24 18:29:13 +0200

    Started dev of resume with automatic scroll page

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -1,8 +1,54 @@
+"use client";
+
+import styles from "@/app/resume/page.module.css";
+import { useEffect, useRef, useState } from "react";
+
 export default function Resume() {
-    
+    const sections = [
+        { id: 1, color: "rgba(255, 0, 0, 0.6)", ref: useRef() },
+        { id: 2, color: "rgba(255, 255, 0, 0.6)", ref: useRef() },
+        { id: 3, color: "rgba(255, 0, 255, 0.6)", ref: useRef() },
+        { id: 4, color: "rgba(0, 255, 0, 0.6)", ref: useRef() },
+        { id: 5, color: "rgba(0, 255, 255, 0.6)", ref: useRef() },
+    ];
+
+    const [currentSection, setCurrentSection] = useState(0);
+
+    useEffect(() => {
+        console.log("currentSection", currentSection);
+        sections[currentSection].ref.current.scrollIntoView();
+    }, [currentSection]);
+
+    useEffect(() => {
+        window.addEventListener("wheel", handleScroll);
+        return () => {
+            window.removeEventListener("wheel", handleScroll);
+        };
+    }, []);
+
+    const handleScroll = (event) => {
+        if (event.deltaY > 0) {
+            setCurrentSection((prevSection) => Math.min(prevSection + 1, sections.length - 1));
+        } else {
+            setCurrentSection((prevSection) => Math.max(prevSection - 1, 0));
+        }
+    };
+
     return (
-        <main>
-            Resume - WIP
-        </main>
-    )
+        <div>
+            {sections.map((section, index) => (
+                <div
+                    ref={section.ref}
+                    key={section.id}
+                    style={{
+                        minHeight: "calc(100vh - 13px)",
+                        backgroundColor: section.color,
+                    }}
+                >
+                    Section {index}
+                    <div style={{ height: index * 500 }}></div>
+                </div>
+            ))}
+        </div>
+    );
 }
