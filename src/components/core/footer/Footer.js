d9d162df6885d40016eeeabb7707dec9e2e757cf
Baptiste Dubillaud <bdubillaud@thinkdeep.ai>
2025-04-14 11:04:13 +0200

    update footer

--- src/components/core/footer/Footer.js
+++ src/components/core/footer/Footer.js
@@ -5,7 +5,7 @@ export default function FooterComponent({}) {
     return (
         <div className={styles.footer_container}>
 
-            <span>Developed in <span className={styles.bold_text}>2024</span> by</span>
+            <span>Developed in <span className={styles.bold_text}>2024-2025</span> by</span>
             <span className={`${styles.orange_text} ${styles.bold_text}`}>Baptiste DUBILLAUD</span>
         </div>
     )
