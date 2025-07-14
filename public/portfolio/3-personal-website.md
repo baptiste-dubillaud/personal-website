a2e06a9ebece287d6f37f4d4a209df1837911f5e
Baptiste Dubillaud <baptiste.dubillaud@gmail.com>
2025-07-14 09:30:41 +0200

    portfolio p2

--- public/portfolio/3-personal-website.md
+++ public/portfolio/3-personal-website.md
@@ -1,7 +1,7 @@
 ---
 title: "Personal Website"
 description: "A personal website hosting my resume, portfolio and blog. Made to get hands on React/Next.js"
-image: "personal-website.png"
+image: "personal-website/home.png"
 technologies: ["React/Next.js", "Hostinger", "Azure"]
 github: "https://github.com/baptiste-dubillaud/personal-website"
 created: "October 2024"
--- src/app/portfolio/[project]/page.js
+++ src/app/portfolio/[project]/page.js
@@ -22,15 +22,10 @@ export default async function Page({ params }) {
                 <Link className={styles.back_to_blog_button} href="/portfolio">
                     {"< Back to Portfolio"}
                 </Link>
-                <div className={styles.title}>{data.title}</div>
                 <div className={styles.writing_data}>
-                    <div className={styles.author_container}>
-                        By <span className={styles.author_name}>{data.author}</span>
-                    </div>
-                    <div className={styles.date_data}>
-                        <div>
-                            Started on <span className={styles.date}>{data.created}</span>
-                        </div>
+                    <div className={styles.title}>{data.title}</div>
+                    <div className={styles.started_date}>
+                        Started on <span className={styles.date}>{data.created}</span>
                     </div>
                 </div>
                 <div className={styles.image_container}>
