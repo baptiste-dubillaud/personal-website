b6081a3d0915feb13768b149ceaf45fa1d5454de
Baptiste Dubillaud <baptiste.dubillaud@gmail.com>
2026-01-16 17:49:58 +0100

    fix docker watch

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        // Enable polling for file changes in Docker/Windows
+        config.watchOptions = {
+            poll: 500,
+            aggregateTimeout: 300,
+            ignored: /node_modules/,
+        };
+        return config;
+    },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
