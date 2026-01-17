5e5e8476d943fea20850e2a4b1405d2c2713af2a
Baptiste Dubillaud <baptiste.dubillaud@gmail.com>
2026-01-17 14:04:26 +0100

    fix watch and live update once and for all

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig = {
-    webpack: (config, { isServer }) => {
-        // Enable polling for file changes in Docker/Windows
-        config.watchOptions = {
-            poll: 500,
-            aggregateTimeout: 300,
-            ignored: /node_modules/,
-        };
-        return config;
-    },
-};
+const nextConfig = {};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
